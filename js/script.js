31e74a81fd49950ea55b7abaf9f47ca1b3746b8e
alegreco7 <alexgreco2002@gmail.com>
2023-12-01 16:33:00 +0100

    esercizio completo

--- js/script.js
+++ js/script.js
@@ -16,6 +16,25 @@ function generateRandomNumbers() {
 function startGame() {
     // Genera i numeri casuali da memorizzare
     const numbersToShow = generateRandomNumbers();
-     // Mostra i numeri all'utente
-     alert("Memorizza i seguenti numeri: " + numbersToShow.join(", "));
+
+    // Mostra i numeri all'utente
+    alert("Memorizza i seguenti numeri: " + numbersToShow.join(", "));
+
+    setTimeout(function() {
+      // Chiedi all'utente di inserire i numeri dopo il tempo di attesa
+      const userNumbers = [];
+      for (let i = 0; i < 5; i++) {
+        const userInput = prompt("Inserisci il numero " + (i + 1));
+        const parsedInput = parseInt(userInput);
+        userNumbers.push(parsedInput);
+      }
+
+      // Verifica i numeri indovinati
+      const correctNumbers = numbersToShow.filter(num => userNumbers.includes(num));
+
+      // Mostra il risultato
+      alert("Hai indovinato " + correctNumbers.length + " numeri: " + correctNumbers.join(", "));
+    }, 5000);
 }
+
+startGame();
